c7460a68fcaa4fd72b4093087e7c3e8b433b18f0
George <xsova113@gmail.com>
2023-02-20 03:13:26 -0800

    - update most of the views - add major functionalities of the app and AVFoundation

---
+++ Scrumdinger/Components/MeetingFooterView.swift
@@ -0,0 +1,52 @@
+//
+//  MeetingFooterView.swift
+//  Scrumdinger
+//
+//  Created by George Chang on 2023-02-19.
+//
+
+import SwiftUI
+
+struct MeetingFooterView: View {
+    let speakers: [ScrumTimer.Speaker]
+    var skipAction: () -> Void
+    
+    private var speakerNumber: Int? {
+        guard let index = speakers.firstIndex(where: { !$0.isCompleted }) else { return nil }
+        return index + 1
+    }
+    private var isLastSpeaker: Bool {
+        return speakers.dropFirst().allSatisfy { $0.isCompleted }
+    }
+    private var speakerText: String {
+        guard let speakerNumber = speakerNumber else { return "No more speakers" }
+        return "Speaker \(speakerNumber) of \(speakers.count)"
+    }
+    
+    var body: some View {
+        VStack {
+            HStack {
+                if isLastSpeaker {
+                    Text("Last Speaker")
+                } else {
+                    Text(speakerText)
+                    Spacer()
+                    Button {
+                        skipAction()
+                    } label: {
+                        Image(systemName: "forward.fill")
+                    }
+                    .accessibilityLabel("Next speaker")
+                }
+            }
+        }
+        .padding([.top, .horizontal])
+    }
+}
+
+struct MeetingFooterView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingFooterView(speakers: DailyScrum.sampleData[0].attendees.speakers, skipAction: {})
+            .previewLayout(.sizeThatFits)
+    }
+}
