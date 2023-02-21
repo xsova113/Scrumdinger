c90136482fe2820b21d165877a38095719e3d8c9
George <xsova113@gmail.com>
2023-02-20 21:54:55 -0800

    - Add ScrumStore class to persist data, save and load data through scene phases.

--- Scrumdinger/Views/ScrumsView.swift
+++ Scrumdinger/Views/ScrumsView.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 struct ScrumsView: View {
     @Binding var scrums: [DailyScrum]
+    @Environment(\.scenePhase) private var scenePhase
     @State private var multiSelection = Set<UUID>()
     @State private var isPresentingNewScrumView = false
     @State private var newScrumData = DailyScrum.Data()
+    let saveAction: () -> Void
     
     var body: some View {
         List($scrums, selection: $multiSelection) { $scrum in
@@ -58,6 +60,9 @@ struct ScrumsView: View {
                         }
                     }
             }
+            .onChange(of: scenePhase) { phase in
+                if phase == .inactive { saveAction() }
+            }
         }
     }
 }
@@ -65,7 +70,7 @@ struct ScrumsView: View {
 struct ScrumsView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationStack {
-            ScrumsView(scrums: .constant(DailyScrum.sampleData))
+            ScrumsView(scrums: .constant(DailyScrum.sampleData), saveAction: {})
         }
     }
 }
