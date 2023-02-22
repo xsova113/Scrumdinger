1277ad72c2fb305d56d3b8d3f8ec7c3cacf1bf3e
George <xsova113@gmail.com>
2023-02-22 02:04:23 -0800

    - Add async await functions for save / load data tasks.

---
+++ Scrumdinger/Models/ErrorWrapper.swift
@@ -0,0 +1,8 @@
+//
+//  ErrorWrapper.swift
+//  Scrumdinger
+//
+//  Created by George Chang on 2023-02-22.
+//
+
+import Foundation
--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -14,22 +14,20 @@ struct ScrumdingerApp: App {
         WindowGroup {
             NavigationStack {
                 ScrumsView(scrums: $store.scrums) {
-                    ScrumStore.save(scrums: store.scrums) { result in
-                        if case .failure(let error) = result {
-                            fatalError(error.localizedDescription)
+                    Task {
+                        do {
+                            try await ScrumStore.save(scrums: store.scrums)
+                        } catch {
+                            fatalError("Error saving scrums")
                         }
                     }
                 }
-                
             }
-            .onAppear {
-                ScrumStore.load { result in
-                    switch result {
-                    case .success(let scrums):
-                        store.scrums = scrums
-                    case .failure(let error):
-                        fatalError(error.localizedDescription)
-                    }
+            .task {
+                do {
+                    store.scrums = try await ScrumStore.load()
+                } catch {
+                    fatalError("Error loading scrums")
                 }
             }
         }
